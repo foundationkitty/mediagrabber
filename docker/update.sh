6b2c18978ae5efc6db8dd4650da20f8685411000
foundationkitty <45774850+foundationkitty@users.noreply.github.com>
2020-02-09 13:59:34 -0800

    Update update.sh

--- docker/update.sh
+++ docker/update.sh
@@ -1,2 +1,21 @@
 #!/bin/bash
 docker images | awk '(NR>1) && ($2!~/none/) {print $1":"$2}' | xargs -L1 docker pull
+cd deezloader
+docker-compose up -d
+cd ../deluge
+docker-compose up -d
+cd ../indexers
+docker-compose up -d
+cd ../minecraftbed
+docker-compose up -d
+cd ../netdata
+docker-compose up -d
+cd ../plex
+docker-compose up -d
+cd ../sabnzbd
+docker-compose up -d
+cd ../scrapers
+docker-compose up -d
+cd ../webbase
+docker-compose up -d
+cd ..
